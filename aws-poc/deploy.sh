97538e2d57aee2e77a7df656f37fd64b677fbb2e
ankittyagi20 <ankittyagi20@gmail.com>
2018-06-18 12:50:31 +0530

    Update deploy.sh

--- aws-poc/deploy.sh
+++ aws-poc/deploy.sh
@@ -7,7 +7,7 @@ sudo apt-get install ansible
 sudo apt-get -y install python-pip
 sudo pip install boto
 
-ansible-playbook -i ansible-environments/hosts ansible-playbooks/provision.yml --extra-vars='env='$1' instance_count='$2' instance_type='$3' username='ankit' password='passcode'' --flush-cache -vvvv
+ansible-playbook -i ansible-environments/hosts ansible-playbooks/provision.yml --extra-vars='env='$1' instance_count='$2' instance_type='$3' username='ankit' password='passcode' --flush-cache -vvvv
 
 ansible-playbook -i ansible-environments/hosts ansible-playbooks/playbook.yml
 
